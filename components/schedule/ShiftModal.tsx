571dbe9e8e05d76df9dd56fd9a5f98d8e387c1fb
root <katharki@stud.ntnu.no>
2026-05-11 15:57:50 +0000

    removed a unused import

--- components/schedule/ShiftModal.tsx
+++ components/schedule/ShiftModal.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useMemo, useRef } from 'react'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { format } from 'date-fns'
+
 import {
   Dialog,
   DialogContent,
